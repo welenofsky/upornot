c6ccd2db98e95b04e3c60ded89970eb9ff99c307
Justin Welenofsky <welenofsky@gmail.com>
2013-07-02 13:21:06 -0700

    Fixed Logic
    One too many while loops. Upped check time from 5 sec intervals to 30
    second intervals.

--- main.py
+++ main.py
@@ -38,35 +38,33 @@ system('title Up or Not')
 
 # Beef of the program, checks for active internet connection.
 def checkI():
-	isDownShown = 0
-	while 1:
-                localtime = time.asctime(time.localtime(time.time()))
-                print(localtime)
-                print("Connecting to External IP...")
-                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                try:
-                        s.connect((IP, port))
-                        s.shutdown(2)
-                        s.close()
-                        print("Online!\n")
-                        if isDownShown == 1:
-                                system('title ONLINE :)')
-                                isDownShown = 0
-                                break
-                except socket.error as e:
-                        print("Cannot connect to ")
-                        print(IP, " on port:", str(port))
-                        print(e)
-                        #Play exclamation sound when internet goes down
-                        winsound.PlaySound("SystemExclamation", winsound.SND_ALIAS)
-                        winsound.PlaySound("SystemExclamation", winsound.SND_ALIAS)
-                        if isDownShown == 0:
-                                system('title OFFLINE')
-                        isDownShown = 1
-                if not args.watch:
-                    exit(0)
-                time.sleep(5);
-while(1):
-        checkI()
-        # Set to re-loop every 30 seconds
-        time.sleep(30)
+    isDownShown = 0
+    localtime = time.asctime(time.localtime(time.time()))
+    print(localtime)
+    print("Connecting to External IP...")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.connect((IP, port))
+        s.shutdown(2)
+        s.close()
+        print("Online!\n")
+        if isDownShown == 1:
+            system('title ONLINE :)')
+            isDownShown = 0
+    except socket.error as e:
+        print("Cannot connect to ")
+        print(IP, " on port:", str(port))
+        print(e)
+        #Play exclamation sound when internet goes down
+        winsound.PlaySound("SystemExclamation", winsound.SND_ALIAS)
+        winsound.PlaySound("SystemExclamation", winsound.SND_ALIAS)
+        if isDownShown == 0:
+            system('title OFFLINE')
+        isDownShown = 1
+    if not args.watch:
+        exit(0)
+        
+while 1:
+    checkI()
+    # Set to re-loop every 30 seconds if --watch passed
+    time.sleep(30)
