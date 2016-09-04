9e7a2cb54a989225865997176b430ce05b02ac68
Justin Welenofsky <welenofsky@gmail.com>
2016-09-04 03:52:55 -0700

    light refactor

--- main.py
+++ main.py
@@ -12,50 +12,15 @@ import time
 import pyglet
 import os
 
-# Argument Parsing
-parser = argparse.ArgumentParser()
-parser.add_argument("-s","-server", help="The IP or Domain Name of the server to monitor")
-parser.add_argument("--watch", action="store_true", help="Watches server until application closes")
-args = parser.parse_args()
-
-# 
-server = args.s
-
-if server == None:
-    print("No IP or Host provided, exiting...")
-    exit(0)
-
-try:
-    IP = socket.gethostbyname(server)
-except socket.gaierror:
-    print("Incorrect server name or IP provided, exiting...")
-    exit(0)
-except:
-    print("Unknown error has occured, check credentials and try again")
-    exit(0)
-
-sound = pyglet.resource.media('alert.wav', streaming=False)
-
-def title_or_echo(title):
-    if os.name == 'nt':
-        system('title ' + title)
-    else:
-        pass
-
-# initalize some more variables
-port = 80
-title_or_echo('Up or Not')
-isOnline = "no"
 
 # Beef of the program, checks for active internet connection.
-def checkI():
+def checkI(IP, port, isOnline):
     if os.name == 'nt':
         os.system('cls')
     else:
         os.system('clear')
 
     isDownShown = 0
-    global isOnline
     localtime = time.asctime(time.localtime(time.time()))
     print("Connecting to External IP...")
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -65,10 +30,10 @@ def checkI():
         s.shutdown(2)
         s.close()
         print("Online!\n")
-        title_or_echo('ONLINE')
+        title_or_pass('ONLINE')
         if isOnline == "no":
             isOnline = "yes"
-            logSys("inetstat",localtime)
+            logSys("inetstat",localtime,isOnline)
     except socket.error as e:
         print("Cannot connect to ")
         print(IP, " on port:", str(port))
@@ -76,17 +41,23 @@ def checkI():
         #Play exclamation sound when internet goes down
         sound.play()
         sound.play()
-        title_or_echo('OFFLINE')
+        title_or_pass('OFFLINE')
         time.sleep(5)
         if isOnline == "yes":
             isOnline = "no"
             logSys("inetstat",localtime)
-            
-    if not args.watch:
-        exit(0)
+
     print("Last check:", localtime)
 
-def logSys(message, timestamp):
+
+def title_or_pass(title):
+    if os.name == 'nt':
+        os.system('title ' + title)
+    else:
+        pass
+
+
+def logSys(message, timestamp, isOnline):
     logfile = open('sysl.txt', 'a')
     if message == "inetstat":
         if isOnline == "yes":
@@ -97,8 +68,45 @@ def logSys(message, timestamp):
         logfile.write("Boot Log - System Awake: " + timestamp + '\n')    
     logfile.closed
 
-logSys("logboot", (time.asctime(time.localtime(time.time()))))
-while 1:
-    checkI()
-    # Set to re-loop every 30 seconds if --watch passed
-    time.sleep(30)
+
+def main():
+    # Argument Parsing
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s","-server", help="The IP or Domain Name of the server to monitor")
+    parser.add_argument("--watch", action="store_true", help="Watches server until application closes")
+    args = parser.parse_args()
+
+    # 
+    server = args.s
+
+    if server == None:
+        print("No IP or Host provided, exiting...")
+        exit(0)
+
+    try:
+        IP = socket.gethostbyname(server)
+    except socket.gaierror:
+        print("Incorrect server name or IP provided, exiting...")
+        exit(0)
+    except:
+        print("Unknown error has occured, check credentials and try again")
+        exit(0)
+
+    sound = pyglet.resource.media('alert.wav', streaming=False)
+
+    # initalize some more variables
+    port = 80
+    title_or_pass('Up or Not')
+    isOnline = "no"
+
+    logSys("logboot", (time.asctime(time.localtime(time.time()))), isOnline)
+    while 1:
+        checkI(IP, port, isOnline)
+        if not args.watch:
+            exit(0)
+
+        # Set to re-loop every 30 seconds if --watch passed
+        time.sleep(30)
+
+
+main()
